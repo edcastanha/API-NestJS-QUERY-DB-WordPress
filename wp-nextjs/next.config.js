a2147ccd43f035ff17161ac2e0367c2e93bcc088
edcastanha <edcastanha@gmail.com>
2021-10-06 23:32:54 -0300

    Init: Projeto inicializado, clear template

---
+++ wp-nextjs/next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  reactStrictMode: true,
+  module: {
+    Image: {
+      domains: ['https://media-exp1.licdn.com/dms/image/C4D03AQG4M0TjZtNZNw/profile-displayphoto-shrink_800_800/0/1625086166431?e=1639008000&v=beta&t=TtrQ4SejsJeAvLdHQgSR-12zpxW27ogkaqm-xwgAQxU'],
+    },
+  }
+}
